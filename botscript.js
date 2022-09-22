f47a5da4338fdbbaa8029e2ff0924e71ad6475ad
SanderLoman <sanderfeitsma13@gmail.com>
2022-09-23 01:19:19 +0200

    commit

--- botscript.js
+++ botscript.js
@@ -1,389 +1,22 @@
 require("dotenv").config()
 const { Contract } = require("ethers")
 const { ethers } = require("hardhat")
+const { ETH_ABI, BNB_ABI, GOE_ABI } = require("./constants.js")
 
 const rpcETH = process.env.ETH_RPC_URL
 const providerETH = new ethers.providers.WebSocketProvider(rpcETH)
 const rpcBNB = process.env.BNB_RPC_URL
 const providerBNB = new ethers.providers.WebSocketProvider(rpcBNB)
+const rpcGOE = process.env.GOE_RPC_URL
+const providerGOE = new ethers.providers.WebSocketProvider(rpcGOE)
 
 const ETH_CONTRACT_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
 const BNB_CONTRACT_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+const GOE_CONTRACT_ADDRESS = "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"
 
-const ETH_CONTRACT_ABI = [
-    {
-        constant: true,
-        inputs: [],
-        name: "name",
-        outputs: [{ name: "", type: "string" }],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: false,
-        inputs: [
-            { name: "_spender", type: "address" },
-            { name: "_value", type: "uint256" },
-        ],
-        name: "approve",
-        outputs: [{ name: "", type: "bool" }],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "totalSupply",
-        outputs: [{ name: "", type: "uint256" }],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: false,
-        inputs: [
-            { name: "_from", type: "address" },
-            { name: "_to", type: "address" },
-            { name: "_value", type: "uint256" },
-        ],
-        name: "transferFrom",
-        outputs: [{ name: "", type: "bool" }],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "decimals",
-        outputs: [{ name: "", type: "uint8" }],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [{ name: "_owner", type: "address" }],
-        name: "balanceOf",
-        outputs: [{ name: "balance", type: "uint256" }],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "symbol",
-        outputs: [{ name: "", type: "string" }],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: false,
-        inputs: [
-            { name: "_to", type: "address" },
-            { name: "_value", type: "uint256" },
-        ],
-        name: "transfer",
-        outputs: [{ name: "", type: "bool" }],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [
-            { name: "_owner", type: "address" },
-            { name: "_spender", type: "address" },
-        ],
-        name: "allowance",
-        outputs: [{ name: "", type: "uint256" }],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    { payable: true, stateMutability: "payable", type: "fallback" },
-    {
-        anonymous: false,
-        inputs: [
-            { indexed: true, name: "owner", type: "address" },
-            { indexed: true, name: "spender", type: "address" },
-            { indexed: false, name: "value", type: "uint256" },
-        ],
-        name: "Approval",
-        type: "event",
-    },
-    {
-        anonymous: false,
-        inputs: [
-            { indexed: true, name: "from", type: "address" },
-            { indexed: true, name: "to", type: "address" },
-            { indexed: false, name: "value", type: "uint256" },
-        ],
-        name: "Transfer",
-        type: "event",
-    },
-]
-const BNB_CONTRACT_ABI = [
-    {
-        anonymous: false,
-        inputs: [
-            {
-                indexed: true,
-                internalType: "address",
-                name: "owner",
-                type: "address",
-            },
-            {
-                indexed: true,
-                internalType: "address",
-                name: "spender",
-                type: "address",
-            },
-            {
-                indexed: false,
-                internalType: "uint256",
-                name: "value",
-                type: "uint256",
-            },
-        ],
-        name: "Approval",
-        type: "event",
-    },
-    {
-        anonymous: false,
-        inputs: [
-            {
-                indexed: true,
-                internalType: "address",
-                name: "from",
-                type: "address",
-            },
-            {
-                indexed: true,
-                internalType: "address",
-                name: "to",
-                type: "address",
-            },
-            {
-                indexed: false,
-                internalType: "uint256",
-                name: "value",
-                type: "uint256",
-            },
-        ],
-        name: "Transfer",
-        type: "event",
-    },
-    {
-        constant: true,
-        inputs: [
-            {
-                internalType: "address",
-                name: "_owner",
-                type: "address",
-            },
-            {
-                internalType: "address",
-                name: "spender",
-                type: "address",
-            },
-        ],
-        name: "allowance",
-        outputs: [
-            {
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: false,
-        inputs: [
-            {
-                internalType: "address",
-                name: "spender",
-                type: "address",
-            },
-            {
-                internalType: "uint256",
-                name: "amount",
-                type: "uint256",
-            },
-        ],
-        name: "approve",
-        outputs: [
-            {
-                internalType: "bool",
-                name: "",
-                type: "bool",
-            },
-        ],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [
-            {
-                internalType: "address",
-                name: "account",
-                type: "address",
-            },
-        ],
-        name: "balanceOf",
-        outputs: [
-            {
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "decimals",
-        outputs: [
-            {
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "getOwner",
-        outputs: [
-            {
-                internalType: "address",
-                name: "",
-                type: "address",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "name",
-        outputs: [
-            {
-                internalType: "string",
-                name: "",
-                type: "string",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "symbol",
-        outputs: [
-            {
-                internalType: "string",
-                name: "",
-                type: "string",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: true,
-        inputs: [],
-        name: "totalSupply",
-        outputs: [
-            {
-                internalType: "uint256",
-                name: "",
-                type: "uint256",
-            },
-        ],
-        payable: false,
-        stateMutability: "view",
-        type: "function",
-    },
-    {
-        constant: false,
-        inputs: [
-            {
-                internalType: "address",
-                name: "recipient",
-                type: "address",
-            },
-            {
-                internalType: "uint256",
-                name: "amount",
-                type: "uint256",
-            },
-        ],
-        name: "transfer",
-        outputs: [
-            {
-                internalType: "bool",
-                name: "",
-                type: "bool",
-            },
-        ],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-    },
-    {
-        constant: false,
-        inputs: [
-            {
-                internalType: "address",
-                name: "sender",
-                type: "address",
-            },
-            {
-                internalType: "address",
-                name: "recipient",
-                type: "address",
-            },
-            {
-                internalType: "uint256",
-                name: "amount",
-                type: "uint256",
-            },
-        ],
-        name: "transferFrom",
-        outputs: [
-            {
-                internalType: "bool",
-                name: "",
-                type: "bool",
-            },
-        ],
-        payable: false,
-        stateMutability: "nonpayable",
-        type: "function",
-    },
-]
+const ETH_CONTRACT_ABI = ETH_ABI
+const BNB_CONTRACT_ABI = BNB_ABI
+const GOE_CONTRACT_ABI = GOE_ABI
 
 // const telegramBot = require("node-telegram-bot-api")
 // const apiKEY = process.env.TG_API_KEY
@@ -405,10 +38,16 @@ let bnbAddresses = new Contract(
     providerBNB
 )
 
+let goeAddresses = new Contract(
+    GOE_CONTRACT_ADDRESS,
+    GOE_CONTRACT_ABI,
+    providerGOE
+)
+
 const addresses = [
-    "0xE4A040AE4bAD72CdC5594695d2f8Efac0fA873cf" /** Jelle zijn wallet*/,
+    "0xE4A040AE4bAD72CdC5594695d2f8Efac0fA873cf" /** Jelle wallet*/,
     "0x55f510bE6AB4c7E07EC6EE637aa83574975D6898" /** random wallet*/,
-    "",
+    "0x7B23298319Ea680e73059AE6eB1fF4162C9bD89e" /** Sander wallet*/,
     "",
     "",
 ]
@@ -430,21 +69,22 @@ const addresses = [
 // })
 
 const main = async () => {
-
     console.log(`Running.
     
-    Waiting for transactions...`);
+    Waiting for transactions...`)
 
-    if (addresses.includes(addresses) === true) {
-        const nameETH = await ethAddresses.name()
-        ethAddresses.on("Transfer", (from, to, amount, data) => {
-            console.log("name:", nameETH)
-            console.log("from:", from)
-            console.log("amount:", amount.toString(), "wei")
-            console.log("to:", to)
-            console.log(`data: https://etherscan.io/tx/${data.transactionHash}\n`)
-        })
-    }
+    const nameETH = await ethAddresses.name()
+    ethAddresses.on("Transfer", (from, to, amount, data) => {
+        //if an address from the array addresses trades an alt coin for ETH, then log that data in the console
+        if (addresses.includes(from)) {
+            console.log(`${nameETH} transfer from: ${from} to: ${to} amount: ${amount} data: ${data}`)
+            // console.log("name:", nameETH)
+            // console.log("from:", from)
+            // console.log("amount:", amount.toString(), "wei")
+            // console.log("to:", to)
+            // console.log(`data: https://etherscan.io/tx/${data.transactionHash}\n`)
+        }
+    })
 
     // providerETH.on("block", async (blockNumber) => {
     //     console.log("New block mined: " + blockNumber)
@@ -456,16 +96,31 @@ const main = async () => {
     //     }
     // })
 
-    if (addresses.includes(addresses === true)) {
-        const nameBNB = await bnbAddresses.name()
-        bnbAddresses.on("Transfer", (from, to, amount, data) => {
-            console.log("name:", nameBNB)
-            console.log("from:", from)
-            console.log("amount:", amount.toString(), "wei")
-            console.log("to:", to)
-            console.log(`data: https://bscscan.com/tx/${data.transactionHash}\n`)
-        })
-    }
+    const nameBNB = await bnbAddresses.name()
+    bnbAddresses.on("Transfer", (from, to, amount, data) => {
+        //if an address from the array addresses trades an alt coin for BNB, then log that data in the console
+        if (addresses.includes(from)) {
+            console.log(`${nameBNB} transfer from: ${from} to: ${to} amount: ${amount} data: ${data}`)
+            // console.log("name:", nameBNB)
+            // console.log("from:", from)
+            // console.log("amount:", amount.toString(), "wei")
+            // console.log("to:", to)
+            // console.log(`data: https://bscscan.com/tx/${data.transactionHash}\n`)
+        }
+    })
+
+    const nameGOE = await goeAddresses.name()
+    goeAddresses.on("Transfer", (from, to, amount, data) => {
+        //if an address from the array addresses trades an alt coin for GOE, then log that data in the console
+        if (addresses.includes(from)) {
+            console.log(`${nameGOE} transfer from: ${from} to: ${to} amount: ${amount} data: ${data}`)
+            // console.log("name:", nameGOE)
+            // console.log("from:", from)
+            // console.log("amount:", amount.toString(), "wei")
+            // console.log("to:", to)
+            // console.log(`data: https://bscscan.com/tx/${data.transactionHash}\n`)
+        }
+    })
 
     // providerBNB.on("block", async (blockNumber) => {
     //     console.log("New block mined: " + blockNumber)
